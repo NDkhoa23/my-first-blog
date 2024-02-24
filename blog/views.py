d897c94f219b6e67e1bff3cc7d04ce198631dba5
nguyenkhoa <nguyenkhoaa888@gmail.com>
2024-02-24 17:26:57 +0700

    Add unpublished posts, buttons for publish and delete posts.

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,12 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
+def publish(self):
+    self.published_date = timezone.now()
+    self.save()
+
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -18,7 +22,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit = False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk = post.pk)
     else:
@@ -31,9 +34,22 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit = False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
